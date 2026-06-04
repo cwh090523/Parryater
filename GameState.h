560a161c4080f609434cb2f70a0165f6f274a008
cwh090523 <cwh090523@gmail.com>
2026-06-04 09:22:44 +0900

    feat : Title

--- GameState.h
+++ GameState.h
@@ -1,23 +1,19 @@
 #pragma once
 #pragma once
-//#include "Enums.h"
+#include "Enums.h"
 #define NOMINMAX
 #include <Windows.h>
 #include <vector>
 
 constexpr int WIDTH = 160;
 constexpr int HEIGHT = 45;
-//
-//struct GameState
-//{
-//	Scene prevScene = Scene::NONE;
-//	Scene curScene = Scene::INGAME;
-//	Menu curMenu = Menu::START;
-//	bool isRunning = true;
-//	Block map[MAP_HEIGHT][MAP_WIDTH] = {};
-//	Player player;
-//	ULONGLONG curTime;
-//	std::vector<Bomb> vecBomb;
-//	std::vector<Flame> vecFlame;
-//	std::vector<DashTrail> vecTrail;
-//};
+
+struct GameState
+{
+	Scene prevScene = Scene::NONE;
+	Scene curScene = Scene::INGAME;
+	Menu curMenu = Menu::START;
+	bool isRunning = true;
+	//Player player;
+	ULONGLONG curTime;
+};
